4d2dd97fdba832fdbede203850119189406cd3ca
Evan Sutherland <stackoverfloweth@gmail.com>
2023-03-19 13:41:53 -0500

    trying different menu option to organize event create

--- src/pages/EventCreatePage.vue
+++ src/pages/EventCreatePage.vue
@@ -1,93 +1,62 @@
 <template>
   <div class="event-create-page">
-    <div v-if="menuVisible" class="event-create-page__sidebar">
+    <div class="event-create-page__title">
       <template v-for="entry in titles" :key="entry.title">
-        <p-overflow-menu-item
+        <p-button
           :to="entry.to"
-          class="event-create-page__sidebar-item"
-          :class="{ 'event-create-page__sidebar-item--active': entry.to.name === route.name }"
-          @click="maybeHideMenu"
+          :flat="entry.to.name !== route.name"
+          class="event-create-page__title-item"
         >
           {{ entry.title }}
-        </p-overflow-menu-item>
+        </p-button>
       </template>
     </div>
-    <div class="event-create-page__content" @click="maybeHideMenu">
-      <div class="event-create-page__title-with-menu">
-        <p-button size="sm" flat icon="MenuIcon" @click.stop="toggleMenu" />
-        <p-bread-crumbs :crumbs="crumbs" />
-      </div>
+    <div class="event-create-page__content">
       <router-view />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { Crumb, media } from '@prefecthq/prefect-design'
-  import { computed, ref } from 'vue'
+  import { computed } from 'vue'
   import { useRoute } from 'vue-router'
   import { routes } from '@/router/routes'
 
   const route = useRoute()
 
-  const menuVisible = ref(media.hover)
-
   const titles = computed(() => [
-    { title: 'Event Information', to: routes.eventsCreateEvent() },
+    { title: 'General', to: routes.eventsCreateGeneral() },
     { title: 'Judging', to: routes.eventsCreateJudging() },
-    { title: 'Registration', to: routes.clubs() },
+    { title: 'Registration', to: routes.eventsCreateRegistration() },
   ])
-
-  const crumbs = computed<Crumb[]>(() => {
-    const activeTitle = titles.value.find(({ to }) => to.name === route.name)
-
-    if (!activeTitle) {
-      return []
-    }
-
-    return [{ text: activeTitle.title }]
-  })
-
-  function toggleMenu(): void {
-    menuVisible.value = !menuVisible.value
-  }
-
-  function maybeHideMenu(): void {
-    if (!media.hover) {
-      menuVisible.value = false
-    }
-  }
 </script>
 
 <style>
 .event-create-page {
   display: flex;
+  flex-direction: column;
   padding: var(--space-4);
   gap: var(--space-4);
   overflow: hidden;
 }
 
-.event-create-page__sidebar {
+.event-create-page__title {
   display: flex;
-  flex-direction: column;
-  flex-shrink: 0;
+  justify-items: start;
+  align-items: start;
+  gap: var(--space-4);
+}
+
+.event-create-page__title-item {
+  font-size: 1rem;
+  line-height: 1.75rem;
+  font-weight: 700;
+  padding: var(--space-3) var(--space-4);
 }
 
 .event-create-page__content {
   display: flex;
   flex-direction: column;
   gap: var(--space-4);
-  flex-grow: 1;
-  flex-shrink: 0;
-}
-
-.event-create-page__title-with-menu {
-  display: flex;
-  align-items: center;
-  gap: var(--space-4);
-}
-
-.event-create-page__sidebar-item--active {
-  color: var(--blue-400);
 }
 </style>
