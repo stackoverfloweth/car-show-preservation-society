605574bf8a5123bfbee87805f98be8b96717e23f
Evan Sutherland <stackoverfloweth@gmail.com>
2023-03-26 12:33:30 -0500

    build editor voting categories table

--- src/components/JudgingCategoriesInputTable.vue
+++ src/components/JudgingCategoriesInputTable.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="judging-categories-input-table">
     <template v-for="category in categories" :key="category.votingCategoryId">
-      <p-list-item-input v-model:selected="selected" :value="category.votingCategoryId" @click="editCategory(category)">
-        {{ category.name }}
-        {{ category.maxCapacity }}
-        {{ category.driversOnly }}
-        {{ category.stripePriceId }}
-        <div class="judging-categories-input-table__actions">
-          <TrashConfirm @confirmed="deleteCategory(category)" />
+      <p-list-item-input
+        v-model:selected="selected"
+        class="judging-categories-input-table__category"
+        :value="category.votingCategoryId"
+        @click="editCategory(category)"
+      >
+        <div class="judging-categories-input-table__name">
+          {{ category.name }}
+        </div>
+        <div class="judging-categories-input-table__description">
+          {{ category.description }}
+        </div>
+        <div class="judging-categories-input-table__info-badges">
+          <template v-if="mocker.create('boolean')">
+            <p-icon-text class="judging-categories-input-table__info-badge" icon="CurrencyDollarIcon">
+              {{ category.stripePriceId ?? '+2.00' }}
+            </p-icon-text>
+          </template>
+          <p-icon-text class="judging-categories-input-table__info-badge" icon="ClipboardCheckIcon">
+            {{ category.driversOnly ? 'drivers only' : 'everyone' }}
+          </p-icon-text>
+          <template v-if="category.maxCapacity">
+            <p-icon-text class="judging-categories-input-table__info-badge" icon="UserGroupIcon">
+              max: {{ category.maxCapacity }}
+            </p-icon-text>
+          </template>
         </div>
       </p-list-item-input>
     </template>
@@ -16,8 +35,8 @@
 
 <script lang="ts" setup>
   import { computed } from 'vue'
-  import TrashConfirm from '@/components/TrashConfirm.vue'
   import { VotingCategory } from '@/models'
+  import { mocker } from '@/services'
 
   const props = defineProps<{
     categories: VotingCategory[],
@@ -54,8 +73,41 @@
   gap: var(--space-2);
 }
 
-.judging-categories-input-table__actions {
+.judging-categories-input-table__category:hover {
+  cursor: pointer;
+  background-color: var(--slate-700);
+}
+
+.judging-categories-input-table__category .p-list-item-input__content {
+  display: grid;
+  grid-template-areas:
+    'name info-badges'
+    'description description';
+  grid-template-columns: minmax(0, 1fr) min-content;
+}
+
+.judging-categories-input-table__name {
+  grid-area: name;
+}
+
+.judging-categories-input-table__description {
+  grid-area: description;
+  color: var(--slate-400);
+  font-size: .75rem;
+  line-height: 0.95rem;
+}
+
+.judging-categories-input-table__info-badges {
   display: flex;
-  gap: var(--space-3);
+  align-items: center;
+  grid-area: info-badges;
+  gap: var(--space-2);
+}
+
+.judging-categories-input-table__info-badge {
+  white-space: nowrap;
+  border-radius: var(--rounded);
+  padding: var(--space-2) var(--space-3);
+  background-color: var(--slate-600);
 }
 </style>
