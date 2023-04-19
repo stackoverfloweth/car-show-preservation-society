b2699fedcd1f939c245c361cabe36c7cdfddcc2b
Evan Sutherland <stackoverfloweth@gmail.com>
2023-04-19 10:00:07 -0500

    separated clubs-list-item like with club-members-list-item

--- src/components/ClubsList.vue
+++ src/components/ClubsList.vue
@@ -2,21 +2,14 @@
   <div class="clubs-list">
     <template v-for="club in clubs" :key="club.clubId">
       <router-link :to="routes.club(club.clubId)">
-        <p-card class="clubs-list__club">
-          <template v-if="club.clubLogo">
-            <SizedImage class="clubs-list__club-logo" :image="club.clubLogo" />
-          </template>
-          <div class="clubs-list__club-name">
-            {{ club.name }}
-          </div>
-        </p-card>
+        <ClubsListItem :club="club" />
       </router-link>
     </template>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import SizedImage from '@/components/SizedImage.vue'
+  import ClubsListItem from '@/components/ClubsListItem.vue'
   import { Club } from '@/models/club'
   import { routes } from '@/router/routes'
 
@@ -30,22 +23,6 @@
   display: grid;
   grid-template-columns: repeat(3, minmax(0, 1fr));
   gap: var(--space-4);
-}
-
-.clubs-list__club {
-  display: flex;
-  flex-direction: column;
-  gap: var(--space-4);
-  align-items: center;
-}
-
-.clubs-list__club-logo {
-  padding-top: 50%;
-  width: 100%;
-}
-
-.clubs-list__club-name {
-  text-align: center;
 }
 
 @media(max-width: 768px){
---
+++ src/components/ClubsListItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <p-card class="clubs-list-item">
+    <template v-if="club.clubLogo">
+      <SizedImage class="clubs-list-item__logo" :image="club.clubLogo" />
+    </template>
+    <div class="clubs-list-item__name">
+      {{ club.name }}
+    </div>
+  </p-card>
+</template>
+
+<script lang="ts" setup>
+  import SizedImage from '@/components/SizedImage.vue'
+  import { Club } from '@/models/club'
+
+  defineProps<{
+    club: Club,
+  }>()
+</script>
+
+<style>
+.clubs-list-item {
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-4);
+  align-items: center;
+}
+
+.clubs-list-item__logo {
+  padding-top: 50%;
+  width: 100%;
+}
+
+.clubs-list-item__name {
+  text-align: center;
+}
+</style>
