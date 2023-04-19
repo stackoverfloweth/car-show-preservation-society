6de4d070604cc81117d64351630987475a833ebd
Evan Sutherland <stackoverfloweth@gmail.com>
2023-04-19 09:58:00 -0500

    separated events-list-item like with club-members-list-item

--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -2,19 +2,14 @@
   <div class="events-list">
     <template v-for="event in events" :key="event.eventId">
       <router-link :to="routes.event(event.eventId)">
-        <p-list-item class="events-list__event">
-          <EventHeader class="events-list__event-header" :event="event" />
-          <p class="events-list__event-location">
-            {{ event.location?.place }}
-          </p>
-        </p-list-item>
+        <EventsListItem :event="event" />
       </router-link>
     </template>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import EventHeader from '@/components/EventHeader.vue'
+  import EventsListItem from '@/components/EventsListItem.vue'
   import { Event } from '@/models/event'
   import { routes } from '@/router/routes'
 
@@ -28,23 +23,5 @@
   display: flex;
   flex-direction: column;
   gap: var(--space-4);
-}
-
-.events-list__event {
-  display: grid;
-  grid-template-areas:
-    'header'
-    'location';
-  gap: var(--space-4);
-}
-
-.events-list__event-header {
-  grid-area: header;
-  pointer-events: none;
-}
-
-.events-list__event-location {
-  grid-area: location;
-  white-space: pre-line;
 }
 </style>
---
+++ src/components/EventsListItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <p-list-item class="events-list-item">
+    <EventHeader class="events-list-item__header" :event="event" />
+    <p class="events-list-item__location">
+      {{ event.location?.place }}
+    </p>
+  </p-list-item>
+</template>
+
+<script lang="ts" setup>
+  import EventHeader from '@/components/EventHeader.vue'
+  import { Event } from '@/models'
+
+  defineProps<{
+    event: Event,
+  }>()
+</script>
+
+<style>
+.events-list-item {
+  display: grid;
+  grid-template-areas:
+    'header'
+    'location';
+  gap: var(--space-4);
+}
+
+.events-list-item__header {
+  grid-area: header;
+  pointer-events: none;
+}
+
+.events-list-item__location {
+  grid-area: location;
+  white-space: pre-line;
+}
+</style>
