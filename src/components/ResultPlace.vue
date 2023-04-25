44423f3aea960fd2b773079031b0fb75d2377e00
Evan Sutherland <stackoverfloweth@gmail.com>
2023-04-24 21:15:01 -0500

    broke out component for results-place

---
+++ src/components/ResultPlace.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="result-place" :class="classes.parent">
+    <p class="result-place__number">
+      <span>{{ place }}</span>
+      <sup class="result-place__ordinal">{{ getOrdinal(place) }}</sup>
+    </p>
+    <component :is="trophy" class="result-place__trophy" :class="classes.trophy" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import trophy from '@/icons/trophy.svg'
+  import { getOrdinal } from '@/utilities'
+
+  const props = defineProps<{
+    place: number,
+  }>()
+
+  const classes = computed(() => ({
+    parent: [`result-place--place-${props.place}`],
+    trophy: [`result-place__trophy--place-${props.place}`],
+  }))
+</script>
+
+<style>
+.result-place {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 60px;
+  padding: var(--space-2);
+  text-align: center;
+}
+
+.result-place--place-1 {
+  color: var(--yellow-400);
+}
+
+.result-place--place-2 {
+  color: var(--zinc-400);
+}
+
+.result-place--place-3 {
+  color: var(--orange-700);
+}
+
+.result-place__trophy--place-1,
+.result-place__trophy--place-2,
+.result-place__trophy--place-3{
+  display: unset !important;
+}
+
+.result-place__trophy {
+  display: none;
+  width: 60px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.result-place__number {
+  font-size: 2rem;
+}
+
+.result-place__ordinal {
+  font-size: 0.7rem;
+  vertical-align: .7em;
+}
+</style>
--- src/components/VotingResultsByEventAndCategoryItem.vue
+++ src/components/VotingResultsByEventAndCategoryItem.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="voting-results-by-event-and-category-item">
-    <div class="voting-results-by-event-and-category-item__place" :class="classes">
-      <p class="voting-results-by-event-and-category-item__place-number">
-        <span>{{ result.placeNumber }}</span>
-        <sup class="voting-results-by-event-and-category-item__place-ordinal">{{ getOrdinal(result.placeNumber) }}</sup>
-      </p>
-      <component :is="trophy" class="voting-results-by-event-and-category-item__trophy" />
-    </div>
+    <ResultPlace :place="result.placeNumber" />
     <div class="voting-results-by-event-and-category-item__body">
       <div class="voting-results-by-event-and-category-item__event">
         {{ event.name }}
@@ -19,18 +13,14 @@
 </template>
 
 <script lang="ts" setup>
-  import { computed } from 'vue'
-  import trophy from '@/icons/trophy.svg'
+  import ResultPlace from '@/components/ResultPlace.vue'
   import { Event, VotingCategory, VotingResult } from '@/models'
-  import { getOrdinal } from '@/utilities'
 
-  const props = defineProps<{
+  defineProps<{
     event: Event,
     votingCategory: VotingCategory,
     result: VotingResult,
   }>()
-
-  const classes = computed(() => [`voting-results-by-event-and-category-item--place-${props.result.placeNumber}`])
 </script>
 
 <style>
@@ -41,51 +31,5 @@
   display: flex;
   align-items: center;
   gap: var(--space-4);
-}
-
-.voting-results-by-event-and-category-item--place-1 .voting-results-by-event-and-category-item__trophy,
-.voting-results-by-event-and-category-item--place-2 .voting-results-by-event-and-category-item__trophy,
-.voting-results-by-event-and-category-item--place-3 .voting-results-by-event-and-category-item__trophy{
-  display: unset;
-}
-
-.voting-results-by-event-and-category-item--place-1 {
-  color: var(--yellow-400);
-}
-
-.voting-results-by-event-and-category-item--place-2 {
-  color: var(--zinc-400);
-}
-
-.voting-results-by-event-and-category-item--place-3 {
-  color: var(--orange-700);
-}
-
-.voting-results-by-event-and-category-item__trophy {
-  display: none;
-  width: 60px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-}
-
-.voting-results-by-event-and-category-item__place {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  width: 60px;
-  padding: var(--space-2);
-  text-align: center;
-}
-
-.voting-results-by-event-and-category-item__place-number {
-  font-size: 2rem;
-}
-
-.voting-results-by-event-and-category-item__place-ordinal {
-  font-size: 0.7rem;
-  vertical-align: .7em;
 }
 </style>
