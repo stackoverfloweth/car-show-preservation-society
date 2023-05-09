392abb7df19822c8f27d842c43034794606c380e
Evan Sutherland <stackoverfloweth@gmail.com>
2023-05-08 21:23:05 -0500

    add link to club editor from empty club photos

--- src/components/ClubPhotoGallery.vue
+++ src/components/ClubPhotoGallery.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="club-photo-gallery">
+    <template v-if="images.length === 0">
+      <p-button :to="routes.clubEditor(clubId)">
+        Add Photos
+      </p-button>
+    </template>
     <PhotoGallery :images="images" :has-more="hasMore" @load-more="loadMore" />
   </div>
 </template>
@@ -8,6 +13,7 @@
   import { toRefs } from 'vue'
   import PhotoGallery from '@/components/PhotoGallery.vue'
   import { useApi, useImageResultsSubscription } from '@/compositions'
+  import { routes } from '@/router/routes'
 
   const props = defineProps<{
     clubId: string,
