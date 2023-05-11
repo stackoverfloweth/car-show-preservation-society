80dcefe2407bb4118102ad4628eb778f67a772e0
Evan Sutherland <stackoverfloweth@gmail.com>
2023-05-10 20:59:36 -0500

    adding jwt decode to netlify webhook

--- netlify/functions/user-webhook.ts
+++ netlify/functions/user-webhook.ts
@@ -1,16 +1,28 @@
 import { Handler } from '@netlify/functions'
+import { JwtRsaVerifier } from 'aws-jwt-verify'
 import { Api, env } from 'netlify/utilities'
 import { getClient } from 'netlify/utilities/mongodbClient'
 
-export const handler: Handler = Api('POST', 'user-webhook', (args, body) => async () => {
+export const handler: Handler = Api('POST', 'user-webhook', (args, body) => async (event) => {
   const client = await getClient()
 
   try {
     const db = client.db(env().mongodbName)
     const collection = db.collection('user')
+    const token = event.headers['X-Webhook-Signature']
+    if (!token) {
+      return { statusCode: 400 }
+    }
 
     await collection.insertOne({
-      ...args, body, env: env().netlifyWebhookSecret,
+      token,
+    })
+
+    const decoder = JwtRsaVerifier.create({ issuer: 'netlify' })
+    const decoded = await decoder.verify(token, { audience: '' })
+
+    await collection.insertOne({
+      decoded,
     })
 
     return { statusCode: 200 }
