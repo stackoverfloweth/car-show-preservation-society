33d210993dcb6d4309b91bc951a3035c48c7ffc4
Evan Sutherland <stackoverfloweth@gmail.com>
2023-05-14 15:46:32 -0500

    using axios dep for http from netlify

--- netlify/utilities/email.ts
+++ netlify/utilities/email.ts
@@ -1,17 +1,19 @@
+import axios from 'axios'
 import { env } from 'netlify/utilities/env'
 
 // eslint-disable-next-line max-params
 export function sendEmail(template: string, from: string, to: string, subject: string, parameters: Record<string, string>): Promise<Response> {
-  return fetch(`${env().url}/.netlify/functions/emails/${template}`, {
+  const instance = axios.create({
+    baseURL: env().url,
     headers: {
       'netlify-emails-secret': env().netlifyEmailsSecret,
     },
-    method: 'POST',
-    body: JSON.stringify({
-      from,
-      to,
-      subject,
-      parameters,
-    }),
+  })
+
+  return instance.post(`/.netlify/functions/emails/${template}`, {
+    from,
+    to,
+    subject,
+    parameters,
   })
 }
